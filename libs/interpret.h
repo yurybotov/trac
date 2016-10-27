9b9e8075dfe4c49b5e0c319e1e731c04d04e5ecf
Yury Botov <yury.botov@gmail.com>
2016-10-27 20:52:17 +0300

    changed on pc

--- libs/interpret.h
+++ libs/interpret.h
@@ -1,6 +1,22 @@
 #ifndef __INTERPRET_H__
 #define __INTERPRET_H__
 
+class Trac {
+  public:
+	Trac(void);
+	~Trac(void);
+
+        void init(void);
+        void run(void);
+	void doStep(void);
+	void feed(char* s);
+	char* out(void);
+  private:
+	void parse(void);
+	void execute(void);
+	void runstdtrac(void);
+	void runuser(void);
+};
 
 
 #endif // __INTERPRET_H__
--- libs/ringbuf.h
+++ libs/ringbuf.h
@@ -17,7 +17,7 @@ public:
 	char pop(void);		// вытолкнуть из конца
 	void put(char c);	// добавить в начало
 	char get(void);		// вытолкнуть из начала
-  void dropfirst(void); // удалить первый символ
+	void dropfirst(void); // удалить первый символ
 
 	char peek(int num); // посмотреть символ с номером num
 
@@ -29,7 +29,7 @@ public:
 
 	char* cutstart(int n); // отрезать и вернуть построку с начала длиной n
 	char* cutend(int n); // отрезать и вернуть построку сконца начиная с символа номер n
-  char* asstring(); // вынуть все из буфера в виде строки
+	char* asstring(); // вынуть все из буфера в виде строки
 
 //#ifdef __DBG__
 //	void dbgprint(void);
