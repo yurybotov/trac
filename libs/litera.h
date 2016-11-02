6d599c2e25905af4c398e24e8e752146e07e2d45
Yury Botov <yury.botov@gmail.com>
2016-11-02 16:26:50 +0300

    dbg13

--- libs/litera.h
+++ libs/litera.h
@@ -8,5 +8,7 @@ int litcmp(litera* d, litera* s);
 int litncmp(litera* d, litera* s, int n);
 litera* litlit(litera* s, litera* sub);
 litera* litchr(litera* s, litera c);
+litera* num2lit(num i, int r);
+litera* int2lit(int i, int r);
 
 #endif // __LITRA_H__
