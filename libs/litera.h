b33b639194c3f11885891efe82bd55413fbd7099
Yury Botov <yury.botov@gmail.com>
2016-11-02 13:51:57 +0300

    dbg6

--- libs/litera.h
+++ libs/litera.h
@@ -1,13 +1,12 @@
-#ifndef __litera_H__
-#define __litera_H__
+#ifndef __LITERA_H__
+#define __LITERA_H__
 
 int litlen(litera* s);
 litera* litcpy(litera* d, litera* s);
 litera* litncpy(litera* d, litera* s, int n);
 int litcmp(litera* d, litera* s);
 int litncmp(litera* d, litera* s, int n);
-int litcpy(litera* d, litera* s);
 litera* litlit(litera* s, litera* sub);
 litera* litchr(litera* s, litera c);
 
-#endif
+#endif // __LITRA_H__
